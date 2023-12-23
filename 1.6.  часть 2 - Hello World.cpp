a7cb41e502235367562c410d460f729ef3fcdbdf
onishchenko71269 <141386539+onishchenko71269@users.noreply.github.com>
2023-12-23 18:43:47 +0300

    Add files via upload
    In this module, you have started to get acquainted with the C++ programming language. We propose to take a very important step in mastering this language — to write your first program. You will have the opportunity to work with the real code and solve the first tasks. At the end of this longrid, you will find hints on completing the task and an example with answers, but we encourage you to complete the tasks yourself first, and only then watch the hint.

---
+++ 1.6.  часть 2 - Hello World.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  std::cout << "=========================\n";
+  std::cout << "II       Hello,        II\n";
+  std::cout << "II       World!        II\n";    
+  std::cout << "=========================\n";
+}
